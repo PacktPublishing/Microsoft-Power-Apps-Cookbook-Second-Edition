113fb0ed0ab163afdfc8b681452a9b6178ef4762
Eickhel Mendoza <17008622+Eickhel@users.noreply.github.com>
2022-03-10 00:05:23 +0000

    Updated Chapter 02 code

---
+++ Chapter02/code/ApplicationCode.js
@@ -0,0 +1,26 @@
+function OpenSidePanel(executionContext) {
+  'use strict';
+  
+  var selectedRecord = executionContext.data.entity;
+  var selectedId = selectedRecord.getId().replace(/[{}]/g, "");
+
+  var pageInput = {
+    pageType: "custom",
+    name: "<logical page name>",
+    entityName: selectedRecord._entityType,
+    recordId: selectedId,
+  };
+  var navigationOptions = {
+    target: 2,
+    position: 2,
+    width: { value: 500, unit: "px" },
+    title: "Gather location",
+  };
+  Xrm.Navigation.navigateTo(pageInput, navigationOptions)
+    .then(function () {
+      executionContext.data.refresh(true);
+    })
+    .catch(function (error) {
+      executionContext.data.refresh(true);
+    });
+}
